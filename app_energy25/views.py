8817701b11af57f86d4c0fc6b74575edc3ceebd5
Emilia Majewska <emilia.majewska27@wp.pl>
2025-05-09 21:20:33 +0200

    Dodanie plików image.png, Vector.svg Zmiany w plikach predkosc_wiatru.html, views.py powodujące powstanie formatki do wyboru miesięcy, oraz powrót do strony głównej

--- app_energy25/views.py
+++ app_energy25/views.py
@@ -5,5 +5,21 @@ def home(request):
     return render(request, 'app_energy25/home_page.html')
 
 def predkosc_wiatru(request):
-
-    return render(request, 'app_energy25/predkosc_wiatru.html')
+    months_list = [
+        "Styczeń",
+        "Luty",
+        "Marzec",
+        "Kwiecień",
+        "Maj",
+        "Czerwiec",
+        "Lipiec",
+        "Sierpień",
+        "Wrzesień",
+        "Październik",
+        "Listopad",
+        "Grudzień",
+    ]
+    context = {
+        'months': months_list,
+    }
+    return render(request, 'app_energy25/predkosc_wiatru.html', context)
